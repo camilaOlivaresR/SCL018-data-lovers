9d660a49f654a12a3c733f82b2f5f8cb2313ef04
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-10-01 20:57:04 -0300

    Nuevos cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import {
-  example
-} from './data.js';
+
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
@@ -27,9 +25,29 @@ for (let i = 0; i < films.length; i++) {
 }
 */
 
+
+const movieList = function(films) {
+  let list = '';
+  //reemplazamos el contenido de filmCard con una cadena vacía
+  document.getElementById('filmCard').innerHTML = '';
+
   for (let i = 0; i < films.length; i++) {
       //template strings
       list += `
   <section class="filmsContainer">
   <figure class="cardImage">                           
-      <img alt="Poster" i
+      <img alt="Poster" id="poster" class="poster" src="${films[i].poster}">
+  </figure>
+  <section class="cardContainer-inner">
+      <h2 id="title" class="cardTitle">${films[i].title}</h2>
+      <h3 id="director" class="cardDirector">Director: ${films[i].director}</h3>
+      <h3 id="score" class="cardYear">(${films[i].rt_score})</h3>
+  </section>
+  </section>`
+  }
+  //el contenido de filmCard ahora contiene los valores de la variable list
+  document.getElementById('filmCard').innerHTML = list;
+}
+movieList(printFilm)
+
+const filmCard = document.getElementById("filmCard");
