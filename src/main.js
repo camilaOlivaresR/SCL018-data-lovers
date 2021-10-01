3da849cf8861ed400233cc3f1dce077da864d54a
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-10-01 17:27:00 -0300

    Experimento

--- src/main.js
+++ src/main.js
@@ -1,30 +1,41 @@
-import data from "./data/ghibli/ghibli.js";
-import orderData from "./data.js";
+import {
+  example
+} from './data.js';
+// import data from './data/lol/lol.js';
+import data from './data/ghibli/ghibli.js';
+// import data from './data/rickandmorty/rickandmorty.js';
 
-let printFilm = data.films;
+//console.log(example, data);
+const printFilm = data.films;
 
-const movieList = function (films) {
-  let list = "";
-  // reemplazamos el contenido de filmCard con una cadena vacía
-  document.getElementById("filmCard").innerHTML = "";
+/*
+const div = document.getElementById('root');
+
+const drawCard = (film) => {
+  div.innerHTML += ` <div>
+      <ol>
+      <li>Titulo: $(film.title)</li>
+      <li>Director: $(film.director)</li>
+      <li>Puntaje: $(film.rt_score)</li>
+      <li>Productor: $(film.producer)</li>
+ </ol>
+ </div>`;
+};
+
+for (let i = 0; i < films.length; i++) {
+  drawCard(films[i]);
+}
+*/
+
+
+const movieList = function(films) {
+  let list = '';
+  //reemplazamos el contenido de filmCard con una cadena vacía
+  document.getElementById('filmCard').innerHTML = '';
 
   for (let i = 0; i < films.length; i++) {
-    //template strings
-    list += `
-    <section class="filmsContainer">
-      <figure class="cardImage">
-        <img alt="Poster" id="poster" class="poster" src="${films[i].poster}">
-      </figure>
-      <section class="cardContainer-inner">
-        <h2 id="title" class="cardTitle">${films[i].title}</h2>
-        <h3 id="director" class="cardDirector">${films[i].director}</h3>
-        <h3 id="rate_score" class="cardRate">${films[i].rt_score}/100</h3>
-      </section>
-    </section>`;
-  }
-  //el contenido de filmCard ahora contiene los valores de la variable list
-  document.getElementById("filmCard").innerHTML = list;
-};
-movieList(printFilm);
-
-const filmCard = document.getElementById("filmCard");
+      //template strings
+      list += `
+  <section class="filmsContainer">
+  <figure class="cardImage">                           
+      <img alt="Poster" i
