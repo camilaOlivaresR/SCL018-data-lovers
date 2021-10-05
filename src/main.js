d8cff79370c6338dc0494a872eb455f4d088af5d
camila <lamica.olivares@gmail.com>
2021-10-04 21:07:44 -0300

    icio de codeo html-js-css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let films = data.films;
+let printMovie= document.getElementById("posterFilms");
+
+const drawCard = (films) => {
+    return `
+     <section class="movieCard">
+        <img alt="Poster" id="poster" src="${films.poster}">
+        <h3>Title: ${films.title}<h3>
+     </section>`
+    };
+
+for (let i=0; i < films.length; i++) {
+    printMovie.innerHTML += drawCard(films[i]);
+}
+
