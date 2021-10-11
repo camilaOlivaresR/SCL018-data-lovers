6a51b15b2b8edccd053b83e497c20c962c60bc43
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2021-10-11 15:47:32 -0300

    Diagrama de flujo

--- src/main.js
+++ src/main.js
@@ -3,5 +3,17 @@ import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+let films = data.films;
+let printMovie= document.getElementById("posterFilms");
 
+const drawCard = (films) => {
+    return `
+     <section class="movieCard">
+        <img alt="Poster" id="poster" src="${films.poster}">
+        <h3>Title: ${films.title}<h3>
+     </section>`
+    };
+
+for (let i=0; i < films.length; i++) {
+    printMovie.innerHTML += drawCard(films[i]);
+}
