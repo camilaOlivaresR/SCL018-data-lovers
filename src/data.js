33647eb78763f777b2b59818f084d7e637761647
unknown <lamica.olivares@gmail.com>
2021-10-14 12:34:47 -0300

    funcion orden

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+export const sortData = (data, sortBy, sortOrder) => {
+  
+  const dataGhibli = data.films;
+  
+  const resultSort = dataGhibli.sort(function(a,b) {
+    if (a[sortBy] > b[sortBy]) {
+      return 1;
+    }
+    if (a[sortBy] > b[sortBy]) {
+      return -1;
+    }
+    return 0;
+  });
+  if (sortOrder ==='') {
+    return resultSort.reverse();
+  }
+else if (sortOrder === '') {
+   return resultSort.reverse
+}
+return resultSort;
+}
--- src/main.js
+++ src/main.js
@@ -4,17 +4,32 @@ import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 let films = data.films;
-let printMovie= document.getElementById("posterFilms");
+let printMovie = document.getElementById("posterFilms");
 
 const drawCard = (films) => {
     return `
      <section class="movieCard">
         <img alt="Poster" id="poster" src="${films.poster}">
-        <h3>Title: ${films.title}<h3>
+      
+        <h4> ${films.title}<h4>
+        <h5> ${films.release_date}<h5>
+       
      </section>`
-    };
+};
 
-for (let i=0; i < films.length; i++) {
+for (let i = 0; i < films.length; i++) {
     printMovie.innerHTML += drawCard(films[i]);
 }
 
+
+/*let scoreOFMovie = document.getElementById("order");
+scoreOFMovie.addEventListener("click", function () {
+    document.getElementsById("posterFilms")[0].innerHTML = "";
+    
+
+
+});*/
+
+const orderOption = document.querySelector("");
+
+orderOption.addEventListener("",(event))
