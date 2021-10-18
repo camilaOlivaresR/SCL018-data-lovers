3d319b4ec6bde5da2f562dbb423319fd717cb47a
mariongarin <mariongarino@gmail.com>
2021-10-18 15:31:17 -0300

    criterios para ordenar en select

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 // estas funciones son de ejemplo
-
+/*
 export const example = () => {
   return 'example';
 };
@@ -7,3 +7,28 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+*/
+
+
+export const sortData = (data, sortBy, sortOrder) => {
+
+const dataGhibli = data.films;
+
+const resultSort = dataGhibli.sort(function (a,b){
+
+  if (a [sortBy] > b [sortBy]) {
+  return -1;
+}
+if (a [sortBy] < b [sortBy]) {
+  return 1;
+}
+return 0;
+});
+
+if (sortOrder === "title") {
+  return resultSort.reverse();
+  }
+  
+return resultSort;
+
+}
--- src/main.js
+++ src/main.js
@@ -1,23 +1,54 @@
-//importación de la data
+/*IMPORTACIÓN DE MÓDULOS*/
+
 import data from './data/ghibli/ghibli.js';
+import {sortData} from "./data.js";
 
-//se establece una constante con las películas de la data
-const films = data.films;
+/*ESTABLECIMIENTO DE VARIABLES*/
+let films = data.films;
+let printMovie= document.getElementById("posterFilms");
+/*let currentFilms = films;*/
 
-//se establece una constante con el contenido del div del html
-const div = document.getElementById("root");
+/*CONFIGURACIÓN DE LAS TARJETAS QUE SE MOSTRARÁN EN EL HTML*/
+const drawCard = (films) => {
+    return `
+     <section class="movieCard">
+     <div class="movieFrame">
+        <img class= "moviePoster" alt="Poster de la película" id="poster" src="${films.poster}">
+        <figcaption class="movieName">${films.title}</figcaption>
+        <figcaption class="movieYear">(${films.release_date})</figcaption>
+        <figcaption class="movieYear">${films.rt_score}</figcaption>
+        </div>
+     </section>`
+    };
 
-//se establece una constante que retorna un template string en el html
-const showingCards = (films) => {
-    return `<div>
-    <ul>
-    <img src= "${films.poster}">
-    <h2>${films.title}</h2>
-    <h2>(${films.release_date})</h2>
-    </ul>
-</div>`;
-};
-//ciclo for que itera dentro del array de film para mostrar cada película en el template string
-for (let i = 0; i < films.length; i++){
-    div.innerHTML += showingCards(films[i]);
+/*FUNCIÓN QUE IMPRIME LAS TARJETAS CON LAS PELÍCULAS
+    const showingMovies = (filmsSelected) => {
+     printedMovies.innerHTML ="";
+     filmsSelected.forEach((currentFilm) => {
+        printedMovies.innerHTML += drawCard(currentFilm);
+     })
+     
+    }
+    showingMovies(films);
+*/
+
+
+for (let i=0; i < films.length; i++) {
+    printMovie.innerHTML += drawCard(films[i]);
 }
+
+
+
+/*const sortBy = "rt_score";*/
+const orderOption = document.querySelector(".combo-box-order");
+
+orderOption.addEventListener("change", (event) => {
+  const chosenOrder = sortData(data, event.target.value, event.target.value);
+  const print = (films) => {
+    printMovie.innerHTML = "";
+    for (let i = 0; i < films.length; i++) {
+      printMovie.innerHTML += drawCard(films[i]);
+    }
+  }
+  print(chosenOrder);
+});
