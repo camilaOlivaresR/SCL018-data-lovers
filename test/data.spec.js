aed3ec7c116ad8fdb74e652bf3ca0b689e429315
unknown <lamica.olivares@gmail.com>
2021-10-28 12:03:05 -0300

    test unitario

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { example, anotherExample } from '../src/data.js';
+/*import { example, anotherExample } from '../src/data.js';
 
 
 describe('example', () => {
@@ -19,5 +19,34 @@ describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
+  });
+});*/
+import { sortData, filterData} from '../src/data.js';
+
+
+describe('sortData', () => {
+  it('is a function', () => {
+    expect(typeof sortData).toBe('function');
+  });
+// se usa toEqual en vez de toBe porque el primero se usa para datos compuestos y el otro para primitivos
+// toEqual y toBe son "matchers" de Jest. Sirven para testear valores. 
+// toEqual compara características y toBe compara identidad exacta. 
+
+  it('returns `an ordered list of films, in descending alphabetical position`', () => {
+    const data = [
+      { title: 'The Cat Returns' },
+      { title: 'Spirited Away' },
+      { title: 'Porco Rosso' },
+      { title: 'My Neighbor Totoro' }
+    ]
+
+    const result = [
+      { title: 'My Neighbor Totoro' },
+      { title: 'Porco Rosso' },
+      { title: 'Spirited Away' },
+      { title: 'The Cat Returns' }
+    ]
+    expect(sortData(data, 'title')).toEqual(result);
+  
   });
 });
